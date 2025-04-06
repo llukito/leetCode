21a02d818ed4dec1d80d26628276e2a03cfe1900
llukito <115102487+llukito@users.noreply.github.com>
2025-04-06 12:22:01 +0400

    Time: 0 ms (100%), Space: 17 MB (8.62%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+         map<int, vector<int>> map2;
+        fillMap(map2, root, 0);
+        vector<vector<int>> vect;
+        for(auto entry : map2){
+            vect.push_back(entry.second);
+        }
+        reverse(vect.begin(),vect.end());
+        return vect;
+    }
+
+    void fillMap(map<int, vector<int>>& map2, TreeNode*& root, int depth){
+        if(root==nullptr)return;
+        map2[depth].push_back(root->val);
+        fillMap(map2, root->left, depth+1);
+        fillMap(map2, root->right, depth+1);
+    }
+};
