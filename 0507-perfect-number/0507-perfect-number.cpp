d14f1e3d1db47ae83fe4e485a0dd254a3b0beb8e
llukito <115102487+llukito@users.noreply.github.com>
2025-05-08 19:55:51 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        // pre computed perfect numbers up to 10^8 which is the constraint
-        unordered_set<int> set = {6, 28, 496, 8128, 33550336};
-        return set.count(num);
+        // // pre computed perfect numbers up to 10^8 which is the constraint
+        // unordered_set<int> set = {6, 28, 496, 8128, 33550336};
+        // return set.count(num);
+        int sum = 1;
+        for(int i=2; i<sqrt(num); i++){
+            if(num%i == 0){
+                sum+=i; sum+=num/i;
+            }
+        }
+        return sum == num;
     }
 };
