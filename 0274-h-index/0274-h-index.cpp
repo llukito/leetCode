f0f8986813e9179558ae380c3c8c24f6375f7ca8
llukito <115102487+llukito@users.noreply.github.com>
2025-05-09 12:22:16 +0400

    Time: 0 ms (100%), Space: 12.3 MB (40.23%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int max_ = citations.size();
+        for(int h = max_; h>0; h--){
+            int index = lower_bound(citations.begin(), citations.end(), h) - citations.begin();
+            if(citations.size() - index >= h)return h;
+        }
+        return 0;
+    }
+};
