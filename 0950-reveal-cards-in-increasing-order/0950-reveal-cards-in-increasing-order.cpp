8eaeeb7ec3302045336d43b16dbf26dc81d109bc
llukito <115102487+llukito@users.noreply.github.com>
2025-05-10 12:57:10 +0400

    Time: 0 ms (100%), Space: 12.6 MB (63.11%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -18,20 +18,4 @@ public:
         return result;
     }
     
-    void fill(vector<int>& deck, vector<int>& result, unordered_set<int>& set, int& last ){
-        if(result.size() == deck.size()){
-            return;
-        }
-        for(int i=0; i<deck.size(); i++){
-            int n = deck[i];
-            int m = deck[(i+1)%deck.size()];
-            if(!set.count(n) && n>last){
-                set.insert(n);
-                result.push_back(n);
-                fill(deck, result, set, n);
-                set.erase(n);
-                result.pop_back();
-            }
-        }
-    }
 };
