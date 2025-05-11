baf11a5a904e586dd70baae448b3a1228872d31a
llukito <115102487+llukito@users.noreply.github.com>
2025-05-11 13:17:25 +0400

    Time: 111 ms (5.01%), Space: 161.9 MB (5%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+       vector<string> res;
+       string curr;
+       fill(res, 2*n, curr);
+        return res;
+    }
+
+    void fill(vector<string>& res, int m, string& curr){
+        if(m == 0){
+            if(valid(curr)){
+                res.push_back(curr);
+            }
+            return;
+        }
+        curr+='(';
+        fill(res, m-1, curr);
+        curr.pop_back();
+        curr+=')';
+        fill(res, m-1, curr);
+        curr.pop_back();
+    }
+
+    bool valid(string& curr){
+        stack<char> st;
+        for(char ch : curr){
+            if(ch == '('){
+                st.push(ch);
+            } else {
+                if(st.empty())return false;
+                if(st.top()!='(')return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
