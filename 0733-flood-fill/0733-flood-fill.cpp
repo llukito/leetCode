46a9566e2bd654a47cff078a9d7ab7717f46daf9
llukito <115102487+llukito@users.noreply.github.com>
2025-05-12 19:21:28 +0400

    Time: 0 ms (100%), Space: 18.3 MB (46.12%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<vector<bool>> bools(image.size(), vector<bool>(image[0].size(), false));
+        check(image, sr, sc, color, image[sr][sc], bools);
+        return image;
+    }
+
+    void check(vector<vector<int>>& image, int sr, int sc, int color, int start, vector<vector<bool>>& bools){
+        image[sr][sc] = color;
+        if(sr+1<image.size() && image[sr+1][sc]==start && !bools[sr+1][sc]){
+            bools[sr+1][sc] = true;
+            check(image, sr+1, sc, color, start, bools);
+        }
+        if(sr-1>=0 && image[sr-1][sc]==start && !bools[sr-1][sc]){
+            bools[sr-1][sc] = true;
+            check(image, sr-1, sc, color, start, bools);
+        }
+        if(sc+1<image[0].size() && image[sr][sc+1]==start && !bools[sr][sc+1]){
+             bools[sr][sc+1] = true;
+            check(image, sr, sc+1, color, start, bools);
+        }
+        if(sc-1>=0 && image[sr][sc-1]==start && !bools[sr][sc-1]){
+             bools[sr][sc-1] = true;
+            check(image, sr, sc-1, color, start, bools);
+        }
+    }
+};
