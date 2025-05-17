2250e3405f53485927cc3e4039ecf4918a139a93
llukito <115102487+llukito@users.noreply.github.com>
2025-05-17 11:28:28 +0400

    Time: 7 ms (0.92%), Space: 21.4 MB (11.8%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,5 +2,40 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         sort(nums.begin(),nums.end());
+        mySort(nums);
+    }
+
+    void mySort(vector<int>& nums){
+        if(nums.size()<=1){
+            return;
+        }
+        vector<int> vect1, vect2;
+        for(int i=0; i<nums.size()/2; i++){
+            vect1.push_back(nums[i]);
+        }
+        for(int i=nums.size()/2; i<nums.size(); i++){
+            vect2.push_back(nums[i]);
+        }
+        mySort(vect1);
+        mySort(vect2);
+        merge(vect1, vect2, nums);
+    }
+
+    void merge(vector<int>& vect1, vector<int>& vect2, vector<int>& nums){
+        deque<int> d1(vect1.begin(), vect1.begin());
+        deque<int> d2(vect2.begin(), vect2.begin());
+        while(!d1.empty() && !d2.empty()){
+            if(d1.front()<d2.front()){
+                nums.push_back(d1.front()); d1.pop_front();
+            } else {
+                nums.push_back(d2.front()); d2.pop_front();
+            }
+        }
+        while(!d1.empty()){
+            nums.push_back(d1.front()); d1.pop_front();
+        }
+        while(!d2.empty()){
+            nums.push_back(d2.front()); d2.pop_front();
+        }
     }
 };
