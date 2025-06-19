f01d70b61e84bc7c8220a0dfcba7f210fcac397b
llukito <115102487+llukito@users.noreply.github.com>
2025-06-19 11:45:40 +0400

    Time: 1634 ms (5.02%), Space: 301 MB (62.21%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        map<int, int> parents;
+        for(int i=0; i<n; i++){
+            parents[i] = i;
+        }
+        for(vector<int> vect : edges){
+            if(find(vect[0], parents)!=find(vect[1], parents)){
+                unify(vect[0], vect[1], parents);
+            }
+        }
+        return find(source, parents) == find(destination, parents);
+    }
+
+    int find(int n, map<int, int>& parents){
+        if(parents[n]!=n){
+            parents[n] = find(parents[n], parents);
+        }
+        return parents[n];
+    }
+
+    void unify(int n, int m, map<int, int>& parents){
+        int p1 = find(n, parents);
+        int p2 = find(m, parents);
+        parents[p1] = p2;
+    }
+};
