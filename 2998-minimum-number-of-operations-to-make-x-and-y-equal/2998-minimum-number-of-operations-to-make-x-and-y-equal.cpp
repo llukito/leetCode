4a32eae52b19330539ad1bf78d071cb23414286f
llukito <115102487+llukito@users.noreply.github.com>
2025-06-19 22:24:34 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2998-minimum-number-of-operations-to-make-x-and-y-equal/2998-minimum-number-of-operations-to-make-x-and-y-equal.cpp
+++ 2998-minimum-number-of-operations-to-make-x-and-y-equal/2998-minimum-number-of-operations-to-make-x-and-y-equal.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     int minimumOperationsToMakeEqual(int x, int y) {
-        if(x < y)return y-x;
-        int U = x-y;
+        if (x < y) {
+            return y - x;
+        }
+        int time = x - y;
+        int U = time+x;
         queue<pair<int,int>> q;
-        q.push({x,0});
-        unordered_set<int> set;
+        q.push({0, x});
         while(!q.empty()){
-            pair<int,int> pr = q.front(); q.pop();
-            int num = pr.first;
-            if(num == y){
-                return min(U, pr.second);
+            int curr = q.front().first;
+            int val = q.front().second;
+            if(val >= U)break;
+            q.pop();
+            if(val == y){
+                time = min(curr, time);
+                break;
             }
-            if(num%11 == 0 && !set.count(num/11)){
-                set.insert(num/11);
-                q.push({num/11, pr.second+1});
+            if(val%11 == 0 && curr+1 < time){
+                q.push({curr+1, val/11});
             }
-            if(num%5 == 0 && !set.count(num/5)){
-                set.insert(num/5);
-                q.push({num/5, pr.second+1});
+            if(val%5 == 0 && curr+1 < time){
+                q.push({curr+1, val/5});
             }
-            if(!set.count(num+1)){
-                set.insert(num+1);
-                q.push({num+1, pr.second+1});
-            }
-            if(!set.count(num-1)){
-                set.insert(num-1);
-                q.push({num-1, pr.second+1});
+            if(curr+1<time){
+                q.push({curr+1, val+1});
+                q.push({curr+1, val-1});
             }
         }
-        return -1;
+        return time;
     }
 };
