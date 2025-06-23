7fca53e0fe86293aeea3cab9893dbb14c294975e
llukito <115102487+llukito@users.noreply.github.com>
2025-06-23 10:09:06 +0400

    Time: 0 ms (100%), Space: 7.8 MB (57.92%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        // // pre computed perfect numbers up to 10^8 which is the constraint
-        // unordered_set<int> set = {6, 28, 496, 8128, 33550336};
-        // return set.count(num);
-
-        if(num<6)return false; // smallest prime number is 6
-        int sum = 1;
-        int sqrtNum = sqrt(num);
-        for(int i=2; i<=sqrtNum; i++){
-            if(num%i == 0){
-                sum+=i;
-                if(i != num/i) sum+= num/i;
-            }
-        }
-        return sum == num;
+        return num == 6 || num == 28 || num == 496 ||
+                num == 8128 || num == 33550336;
     }
 };
