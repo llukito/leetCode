851c7ea5672ab270059e772563315692e9246790
llukito <115102487+llukito@users.noreply.github.com>
2025-06-23 10:25:19 +0400

    Time: 6 ms (26.01%), Space: 9.6 MB (91.2%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,23 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // sort(s.begin(), s.end());
-        // sort(t.begin(), t.end());
-        // return s == t;
-        if(s.length()!=t.length())return false;
-        map<char, int> mp1;
-        for(char ch : s){
-            mp1[ch]++;
-        }
-        map<char, int> mp2;
-        for(char ch : t){
-            mp2[ch]++;
-        }
-        for(auto& entry : mp1){
-            if(!mp2.count(entry.first) || mp2[entry.first] != entry.second){
-                return false;
-            }
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
     }
 };
