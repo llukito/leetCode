96e505bf24a2bfc16ce6f7588a3c207f1b08f4aa
llukito <115102487+llukito@users.noreply.github.com>
2025-06-23 10:15:05 +0400

    Time: 0 ms (100%), Space: 7.8 MB (57.92%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        return num == 6 || num == 28 || num == 496 ||
-                num == 8128 || num == 33550336;
+        if(num == 1)return false;
+        int sum = 1;
+        for(int i=2; i<=sqrt(num); i++){
+            if(num%i==0){
+                sum+=i+num/i;
+            }
+        }
+        return sum == num;
     }
 };
+
+
+// bool checkPerfectNumber(int num) {
+//     return num == 6 || num == 28 || num == 496 ||
+//             num == 8128 || num == 33550336;
+// }
