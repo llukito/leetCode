218884c557c87b24d9a4fbbe9161441d9661c7a5
llukito <115102487+llukito@users.noreply.github.com>
2025-06-24 12:34:59 +0400

    Time: 4 ms (6.66%), Space: 18.2 MB (49.95%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,3 +1,4 @@
+// leetcode number 733 (Flood Fill)
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
@@ -6,23 +7,23 @@ public:
         return image;
     }
 
-    void check(vector<vector<int>>& image, int sr, int sc, int color, int start, vector<vector<bool>>& bools){
+    void check(vector<vector<int>>& image, int sr, int sc, int color, int start, vector<vector<bool>>& bools) {
         image[sr][sc] = color;
-        if(sr+1<image.size() && image[sr+1][sc]==start && !bools[sr+1][sc]){
-            bools[sr+1][sc] = true;
-            check(image, sr+1, sc, color, start, bools);
+        if (sr + 1 < image.size() && image[sr + 1][sc] == start && !bools[sr + 1][sc]) {
+            bools[sr + 1][sc] = true;
+            check(image, sr + 1, sc, color, start, bools);
         }
-        if(sr-1>=0 && image[sr-1][sc]==start && !bools[sr-1][sc]){
-            bools[sr-1][sc] = true;
-            check(image, sr-1, sc, color, start, bools);
+        if (sr - 1 >= 0 && image[sr - 1][sc] == start && !bools[sr - 1][sc]) {
+            bools[sr - 1][sc] = true;
+            check(image, sr - 1, sc, color, start, bools);
         }
-        if(sc+1<image[0].size() && image[sr][sc+1]==start && !bools[sr][sc+1]){
-             bools[sr][sc+1] = true;
-            check(image, sr, sc+1, color, start, bools);
+        if (sc + 1 < image[0].size() && image[sr][sc + 1] == start && !bools[sr][sc + 1]) {
+            bools[sr][sc + 1] = true;
+            check(image, sr, sc + 1, color, start, bools);
         }
-        if(sc-1>=0 && image[sr][sc-1]==start && !bools[sr][sc-1]){
-             bools[sr][sc-1] = true;
-            check(image, sr, sc-1, color, start, bools);
+        if (sc - 1 >= 0 && image[sr][sc - 1] == start && !bools[sr][sc - 1]) {
+            bools[sr][sc - 1] = true;
+            check(image, sr, sc - 1, color, start, bools);
         }
     }
 };
