3981699a0eb754952e577cf17b36c6db6c416b9b
llukito <115102487+llukito@users.noreply.github.com>
2025-06-24 12:26:53 +0400

    Time: 122 ms (5.03%), Space: 161.9 MB (5%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,35 +1,37 @@
+// leetcode number 22 (Generate Parentheses)
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
-       vector<string> res;
-       string curr;
-       fill(res, 2*n, curr);
+        vector<string> res;
+        string curr;
+        fill(res, 2 * n, curr);
         return res;
     }
 
-    void fill(vector<string>& res, int m, string& curr){
-        if(m == 0){
-            if(valid(curr)){
+    void fill(vector<string>& res, int m, string& curr) {
+        if (m == 0) {
+            if (valid(curr)) {
                 res.push_back(curr);
             }
             return;
         }
-        curr+='(';
-        fill(res, m-1, curr);
+        curr += '(';
+        fill(res, m - 1, curr);
         curr.pop_back();
-        curr+=')';
-        fill(res, m-1, curr);
+        curr += ')';
+        fill(res, m - 1, curr);
         curr.pop_back();
     }
 
-    bool valid(string& curr){
+    bool valid(string& curr) {
         stack<char> st;
-        for(char ch : curr){
-            if(ch == '('){
+        for (char ch : curr) {
+            if (ch == '(') {
                 st.push(ch);
-            } else {
-                if(st.empty())return false;
-                if(st.top()!='(')return false;
+            }
+            else {
+                if (st.empty())return false;
+                if (st.top() != '(')return false;
                 st.pop();
             }
         }
