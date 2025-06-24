476a9add9d658dc7ea67978e6e518a634ad0a0a3
llukito <115102487+llukito@users.noreply.github.com>
2025-06-24 12:34:00 +0400

    Time: 199 ms (6.82%), Space: 71.5 MB (5.19%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,23 @@
+// leetcode number 322 (Coin Change)
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-       queue<pair<int,int>> q;
-       q.push({0, 0});
-       unordered_set<int> st;
-       while(!q.empty()){
+        queue<pair<int, int>> q;
+        q.push({ 0, 0 });
+        unordered_set<int> st;
+        while (!q.empty()) {
             pair<int, int> pr = q.front(); q.pop();
             long long num = pr.first;
-            if(num == amount){
+            if (num == amount) {
                 return pr.second;
             }
-            for(int n : coins){
-                if(num+n <= amount && !st.count(num+n)){
-                    st.insert(num+n);
-                    q.push({num+n, pr.second+1});
+            for (int n : coins) {
+                if (num + n <= amount && !st.count(num + n)) {
+                    st.insert(num + n);
+                    q.push({ num + n, pr.second + 1 });
                 }
             }
-       } 
-       return -1;
+        }
+        return -1;
     }
 };
