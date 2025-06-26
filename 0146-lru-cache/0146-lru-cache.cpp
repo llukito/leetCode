263c1bb65683b6b55beba8dd527e49ac08586e49
llukito <115102487+llukito@users.noreply.github.com>
2025-06-26 20:24:03 +0400

    Time: 49 ms (96.17%), Space: 173 MB (80.65%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,40 +1,74 @@
 class LRUCache {
 public:
+    struct Node{
+        int val;
+        int key;
+        Node* next;
+        Node* prev;
+    };
 
-    int size=0;
-    list<pair<int, int>> lruList;
-    unordered_map<int, list<pair<int, int>>::iterator> map;
+    unordered_map<int, Node*> map;
+    int size = 0;
+    int loglen = 0;
+    Node* head = nullptr;
+    Node* tail = nullptr;
 
     LRUCache(int capacity) {
         size = capacity;
+        head = new Node();
+        tail = new Node();
+        head->prev = tail->next = nullptr;
+        head->next = tail; tail->prev = head;
     }
     
     int get(int key) {
         if(map.count(key)){
-            auto t = map[key];
-            int val = t->second;
-            lruList.erase(t);
-            lruList.push_front({key, val});
-            map[key] = lruList.begin();
-            return val;
+            int res = map[key]->val;
+
+            Node*& nd = map[key];
+            nd->prev->next = nd->next;
+            nd->next->prev = nd->prev;
+
+            tail->prev->next = nd;
+            nd->prev = tail->prev;
+            nd->next = tail;
+            tail->prev = nd;
+            return res;
         }
         return -1;
     }
     
     void put(int key, int value) {
         if(map.count(key)){
-            auto t = map[key];
-            int val = t->second;
-            lruList.erase(t);
-            lruList.push_front({key, value});
-            map[key] = lruList.begin();
+            Node*& nd = map[key];
+            nd->prev->next = nd->next;
+            nd->next->prev = nd->prev;
+
+            nd->val = value;
+            tail->prev->next = nd;
+            nd->prev = tail->prev;
+            nd->next = tail;
+            tail->prev = nd;
         } else {
-            if(lruList.size() == size){
-                map.erase(lruList.back().first);
-                lruList.pop_back();
+            if(loglen == size){
+                Node* nd = head->next;
+                if(nd->next){
+                    head->next = nd->next;
+                    nd->next->prev = head;
+                }
+                map.erase(nd->key);
+                delete nd;
+                loglen--;
             }
-            lruList.push_front({key, value});
-            map[key] = lruList.begin();
+
+            Node* nd = new Node(); nd->val = value; nd->key = key;
+            tail->prev->next = nd;
+            nd->prev = tail->prev;
+            nd->next = tail;
+            tail->prev = nd;
+
+            loglen++;
+            map[key] = nd;
         }
     }
 };
