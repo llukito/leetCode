e47aca0b7b578b8845bbfb7897f2240b544311d3
llukito <115102487+llukito@users.noreply.github.com>
2025-07-05 17:57:09 +0400

    Time: 4 ms (35.35%), Space: 16.2 MB (87.25%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -2,32 +2,38 @@ class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
         int n = board.size();
-        queue<pair<int,int>> q;
-        q.push({1, 0});
-         vector<bool> visited(n * n + 1, false);
-         visited[0] = true;
-        while(!q.empty()){
-            int curr = q.front().first; int steps = q.front().second; q.pop();
-            if(curr == n*n){
-                return steps;
-            }
-            for(int i=curr+1; i<=min(curr+6, n*n); i++){
-                 int r = n - 1 - ((i - 1) / n);
-                int c = ( (i - 1) % n );
+        queue<pair<int, int>> q;
+        vector<bool> visited(n * n + 1, false);
 
-                if ( (( (i - 1) / n ) % 2 ) == 1 ) {
-                    c = n - 1 - c;
+        q.push({1, 0});
+        visited[1] = true;
+
+        while (!q.empty()) {
+            auto [cell, moves] = q.front(); q.pop();
+
+            if (cell == n * n) return moves;
+
+            for (int i = 1; i <= 6 && cell + i <= n * n; ++i) {
+                int next = cell + i;
+                auto [r, c] = getPosition(next, n);
+                if (board[r][c] != -1) {
+                    next = board[r][c];
                 }
-                if(!visited[i]){
-                    visited[i] = true;
-                    if (board[r][c] == -1) {
-                    q.push({ i, steps + 1 });
-                } else {
-                    q.push({ board[r][c], steps + 1 });
-                }
+                if (!visited[next]) {
+                    visited[next] = true;
+                    q.push({next, moves + 1});
                 }
             }
         }
+
         return -1;
+    }
+
+    pair<int, int> getPosition(int num, int n) {
+        int quot = (num - 1) / n;
+        int rem = (num - 1) % n;
+        int row = n - 1 - quot;
+        int col = (quot % 2 == 0) ? rem : n - 1 - rem;
+        return {row, col};
     }
 };
