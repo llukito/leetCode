1416d6e2522e3f38f4fc7996fe35924dafd2d4ab
llukito <115102487+llukito@users.noreply.github.com>
2025-07-06 15:12:08 +0400

    Time: 0 ms (100%), Space: 17.5 MB (77%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -1,43 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        if(!root)return;
-        TreeNode* head = nullptr;
-        TreeNode* tail = nullptr;
-        fill(root->left, head, tail);
-        root->left = nullptr;
+        check(root);
+    }
+
+    pair<TreeNode*, TreeNode*> check(TreeNode* root){
+        if(!root){
+            return {nullptr, nullptr};
+        }
+        if(!root->left && !root->right){
+            return {root,root};
+        }
         TreeNode* temp = root->right;
-        root->right = head; 
-
-        TreeNode* head2 = nullptr;
-        TreeNode* tail2 = nullptr;
-        fill(temp, head2, tail2);
-        if(tail){
-            tail->right = head2; 
-        } else {
-            root->right = head2;
+        TreeNode* temp1 = root->left;
+        pair<TreeNode*, TreeNode*> pr{nullptr, nullptr};;
+        if(root->left){
+            pr = check(root->left);
+            root->right = pr.first;
+            root->left = nullptr;
         }
-    }
-    
-    void fill(TreeNode* root, TreeNode*& head, TreeNode*& tail) {
-        if (!root) return;
-        TreeNode* nd = new TreeNode();
-        nd->val = root->val; nd->left = nullptr; nd->right = nullptr;
-        if(!head){
-            head = nd;
-            tail = nd;
-        } else {
-            tail->right = nd;
-            tail = nd;
+         pair<TreeNode*, TreeNode*> pr2 = {nullptr, nullptr};
+        if(temp){
+            pr2 = check(temp);
+            if(pr.second){
+                pr.second->right = pr2.first;
+                pr.second->left = nullptr;
+            }
         }
-        fill(root->left, head, tail);
-        fill(root->right, head, tail);
-    }
-
-    void freeTree(TreeNode*& root){
-        if(!root)return;
-        freeTree(root->left);
-        freeTree(root->right);
-        delete root;
+        return {root, pr2.second ? pr2.second : pr.second ? pr.second : root };
     }
 };
