ea48cbb04d306f25319dab010717f3cf692d3a77
Luka Aladashvili <115102487+llukito@users.noreply.github.com>
2025-08-10 12:50:20 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        map<int,int> dp;
+        dp[0] = nums[0];
+        dp[1] = max(nums[1], dp[0]);
+        for(int i=2; i<nums.size(); i++){
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+        }
+        return dp[nums.size()-1];
+    }
+};
