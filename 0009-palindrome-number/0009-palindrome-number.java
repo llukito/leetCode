57b1bc6df2b9cb53b43f2b6ca42d303541add37a
Luka Aladashvili <115102487+llukito@users.noreply.github.com>
2025-12-08 12:45:33 +0400

    Update 0009-palindrome-number.java

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0)return false;
         long savedX = x; long newNumber = 0;
-        while(x!=0){
+        while( x!=0 ){
             int n = x%10;
             newNumber*=10; 
             newNumber+=n;
