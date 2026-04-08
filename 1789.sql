622c04e30e10192e2133997fde563d0c7df37527
Luka Aladashvili <115102487+llukito@users.noreply.github.com>
2026-04-08 12:22:39 +0400

    Add SQL query to select employees by primary flag
    Retrieve employee IDs and department IDs based on primary flag status.

---
+++ 1789.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT employee_id, department_id 
+FROM Employee
+WHERE primary_flag = 'Y'
+
+UNION
+
+SELECT employee_id, department_id 
+FROM Employee
+WHERE primary_flag = 'N' AND employee_id NOT IN 
+(SELECT employee_id
+FROM Employee
+WHERE primary_flag = 'Y')
